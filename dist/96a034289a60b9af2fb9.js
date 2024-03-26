d0308ae6558ec37241ac218d4a1522f1605b2b28
a.miyazaki <a.takahashi@flag-pictures.co.jp>
2024-03-26 11:23:58 +0900

    Display adjustment

---
+++ dist/96a034289a60b9af2fb9.js
@@ -0,0 +1,1 @@
+import"./main.scss";$((function(){$(".js-game").hide(),$(".js-btn-start").on("click",(function(){$(".js-start").fadeOut(),$(".js-game").fadeIn()}))}));
--- src/index.js
+++ src/index.js
@@ -5,5 +5,29 @@ $(function(){
   $('.js-btn-start').on('click', function(){
     $('.js-start').fadeOut();
     $('.js-game').fadeIn();
+
+    // timer
+    let sec = 0;
+    let min = 0;
+
+    let timer;
+
+    sec = 0;
+    min = 0;
+
+    $('.js-timer').html('00:00');
+    // timer = setInterval(countup, 1000);
   });
+
+  // function countup(){
+  //   sec += 1;
+
+  //   if(sec > 59){
+  //     sec = 0;
+  //     min += 1;
+  //   }
+
+  //   $('.js-timer').html(min + ':' + sec);
+  // }
+
 });
